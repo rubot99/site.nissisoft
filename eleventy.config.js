b160bf1704e0248185663cb55271b548b8d24418
Reuben <helix99@gmail.com>
2023-11-13 19:06:54 +0000

    added page

---
+++ eleventy.config.js
@@ -0,0 +1,42 @@
+const { DateTime } = require("luxon");
+
+const pluginImages = require("./eleventy.config.images.js");
+
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy({
+		"./public/": "/"
+	});
+
+	eleventyConfig.addPlugin(pluginImages);
+
+	eleventyConfig.addFilter("readableDate", (dateObj, format, zone) => {
+		// Formatting tokens for Luxon: https://moment.github.io/luxon/#/formatting?id=table-of-tokens
+		return DateTime.fromJSDate(dateObj, { zone: zone || "utc" }).toFormat(format || "dd LLLL yyyy");
+	});
+
+	eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+		// dateObj input: https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+		return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+	});
+
+	eleventyConfig.addFilter("filterTagList", function filterTagList(tags) {
+		return (tags || []).filter(tag => ["all", "nav", "post", "posts"].indexOf(tag) === -1);
+	});
+
+	eleventyConfig.addFilter("head", (array, n) => {
+		if(!Array.isArray(array) || array.length === 0) {
+			return [];
+		}
+		if( n < 0 ) {
+			return array.slice(n);
+		}
+
+		return array.slice(0, n);
+	});
+	
+	return {
+		dir: {
+		  input: 'src'
+		}
+	  };
+};
